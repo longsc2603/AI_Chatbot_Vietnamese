ad29f1236cb05b33c65b0c1781f163e6e09b9cd6
longsc2603 <56826526+longsc2603@users.noreply.github.com>
2022-02-10 16:00:17 +0700

    Update training_phobert.py

--- training_phobert.py
+++ training_phobert.py
@@ -8,13 +8,16 @@ from phobert_finetuned import PhoBERT_finetuned
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
-# Load data
+# Load train and validation dataset
 with open('content.json', 'r', encoding="utf-8") as c:
     contents = json.load(c)
+with open('val_content.json', 'r', encoding="utf-8") as v:
+    val_contents = json.load(v)
 # Load model PhoBERT and its tokenizer
 phobert = AutoModel.from_pretrained('vinai/phobert-base')
 tokenizer = AutoTokenizer.from_pretrained('vinai/phobert-base')
 
+# Process the train dataset:
 tags = []
 X = []
 y = []
@@ -33,7 +36,7 @@ for tag in tags:
 token_train = {}
 token_train = tokenizer.batch_encode_plus(
     X,
-    max_length=15,
+    max_length=13,
     padding='max_length',
     truncation=True
 )
@@ -41,16 +44,44 @@ X_train_mask = torch.tensor(token_train['attention_mask'])
 X_train = torch.tensor(token_train['input_ids'])
 y_train = torch.tensor(y)
 
+# Process the validation dataset:
+tags_val = []
+X_val = []
+y_val = []
+
+for val_content in val_contents['intents']:
+    tag_val = val_content['tag']
+    for val_pattern in val_content['patterns']:
+        X_val.append(val_pattern)
+        tags_val.append(tag_val)
+
+for tag_val in tags_val:
+    label = tags_set.index(tag_val)
+    y_val.append(label)
+token_val = {}
+token_val = tokenizer.batch_encode_plus(
+    X_val,
+    max_length=13,
+    padding='max_length',
+    truncation=True
+)
+X_val_mask = torch.tensor(token_val['attention_mask'])
+X_val = torch.tensor(token_val['input_ids'])
+y_val = torch.tensor(y_val)
+
 # Hyperparameter:
 batch_size = 8
 hidden_size = 512
-num_class = len(tags)
+num_class = len(tags_set)
 lr = 7.5e-5
 num_epoch = 100
 
 dataset = TensorDataset(X_train, X_train_mask, y_train)
 train_data = DataLoader(dataset=dataset, batch_size=batch_size,
                         shuffle=True)
+val_dataset = TensorDataset(X_val, X_val_mask, y_val)
+val_data = DataLoader(dataset=val_dataset, batch_size=batch_size,
+                      shuffle=True)
 
 # Model:
 for param in phobert.parameters():
@@ -63,6 +94,7 @@ loss_f = nn.NLLLoss()
 
 
 def train():
+    print("Training...")
     model.train()
     total_loss = 0
     for (step, batch) in enumerate(train_data):
@@ -80,10 +112,47 @@ def train():
         torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
         optimizer.step()
     avg_loss = total_loss/len(train_data)
-    print(avg_loss)
+    return avg_loss
 
+
+def evaluate():
+    print("Evaluating...")
+    # deactivate dropout layers
+    model.eval()
+    total_loss = 0
+    # iterate over batches
+    for (step, batch) in enumerate(val_data):
+        # push the batch to gpu
+        batch = [t.to(device) for t in batch]
+        sent_id_val, mask_val, labels_val = batch
+
+        # deactivate autograd
+        with torch.no_grad():
+            # model predictions
+            preds = model(sent_id_val, mask_val)
+            # compute the validation loss between actual and predicted values
+            loss = loss_f(preds, labels_val)
+            total_loss = total_loss + loss.item()
+            preds = preds.detach().cpu().numpy()
+
+    # compute the validation loss of the epoch
+    avg_loss = total_loss / len(val_data)
+    return avg_loss
+
+
+best_valid_loss = float('inf')
+train_losses = []
+valid_losses = []
 
 for epoch in range(num_epoch):
     print('\n Epoch {:}/{:}'.format(epoch + 1, num_epoch))
-    train()
-torch.save(model.state_dict(), 'saved_weights.pth')
+    train_loss = train()
+    # evaluate model
+    valid_loss = evaluate()
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'saved_weights.pth')
+    train_losses.append(train_loss)
+    valid_losses.append(valid_loss)
+    print(f'\nTraining Loss: {train_loss:.3f}')
+    print(f'Validation Loss: {valid_loss:.3f}')
